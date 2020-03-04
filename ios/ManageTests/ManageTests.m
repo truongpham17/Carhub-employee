f4a58fc3604f8ff7eca3ef27fba0c8b93131a0f3
Duong Hoang <phamminhtruongpro17@gmail.com>
2020-03-04 08:53:16 +0700

    modified project name and bundle identifier

--- ios/ManageTests/ManageTests.m
+++ ios/ManageTests/ManageTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CarhubTests : XCTestCase
+@interface ManageTests : XCTestCase
 
 @end
 
-@implementation CarhubTests
+@implementation ManageTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
